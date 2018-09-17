e1f485f9c557813484775838dcab71072671b362
ant <3968204@gmail.com>
2018-09-17 12:42:35 +0300

    Add input of two numbers. Refactoring.

--- prime_numbes.py
+++ prime_numbes.py
@@ -1,26 +1,48 @@
+"""
+HW -- 5.
 
-def prime_numbers(number):
-    sums = 0
-    if number <= 0:
-        print('input positive number')
+Output sum of prime numbers in the specified range [n, m).
+Use concurrent.futures or/and asyncio.
+"""
 
-    for prime_number in range(number + 1):
-        if prime_number == 1:
-            continue
+import asyncio
 
-        is_prime = True
-        for num in range(2, int(prime_number ** 0.5) + 1):
-            if prime_number % num == 0:
-                is_prime = False
-                break
 
-        if is_prime:
-            sums += prime_number
-            print(prime_number)
+async def sum_prime_numbers(left_border, right_border):
+    """Asynchronously summarize prime numbers."""
+    prime_sum = 0
+    if left_border <= 0 or right_border <= 0:
+        print('Please input positive numbers.')
+    elif left_border >= right_border:
+        print('The first number should be less than the second.')
+    else:
+        for prime_number in range(left_border, right_border + 1):
+            if prime_number == 1:
+                continue
 
-    print('Sum is {}'.format(sums))
+            is_prime = await check_prime_number(prime_number)
+            if is_prime:
+                print(prime_number)
+                prime_sum += prime_number
+
+        print('Sum is {}.'.format(prime_sum))
+
+
+async def check_prime_number(prime_number):
+    """Check if number is prime."""
+    is_prime = True
+    for num in range(2, int(prime_number ** 0.5) + 1):
+        if prime_number % num == 0:
+            is_prime = False
+            break
+    return is_prime
+
 
 if __name__ == '__main__':
-    print('Print positive number to calculate sum of prime numbers')
-    num = input('Your number: ')
-    prime_numbers(int(num))
+    print('Calculate sum of prime numbers')
+    first_num = int(float(input('First number: ')))
+    second_num = int(float(input('Second number: ')))
+
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(sum_prime_numbers(first_num, second_num))
+    loop.close()
