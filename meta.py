24d5cc2a576534d6ed5340aef62117c5297c1a9b
ant <3968204@gmail.com>
2018-09-27 17:15:48 +0300

    Refactoring.

--- meta.py
+++ meta.py
@@ -1,59 +1,50 @@
+"""
+HW -- 8.
+
+Metaclass implemetation.
+"""
+
 import re
 
 
 class MetaNew(type):
+    """Metaclass to convert get_*/ set_* methods to property."""
+
     def __new__(cls, name, bases, classdict):
+        """__new__ method."""
         get_regexp = r'\bget_\w+'
         set_regexp = r'\bset_\w+'
 
         temp_classdict = {}
-        for class_obj in classdict:
-            if re.fullmatch(get_regexp, class_obj):
-                new_attr = class_obj.split('_')[1]
-                if temp_classdict.get(new_attr):
-                    descrips= cls._get_descriptors(temp_classdict[new_attr])
-                    descrips[0] = classdict[class_obj]
-                    temp_classdict[new_attr] = property(*descrips)
-                else:
-                    temp_classdict[new_attr] = property(classdict[class_obj])
-            elif re.fullmatch(set_regexp, class_obj):
-                new_attr = class_obj.split('_')[1]
-                if temp_classdict.get(new_attr):
-                    descrips= cls._get_descriptors(temp_classdict[new_attr])
-                    descrips[1] = classdict[class_obj]
-                    temp_classdict[new_attr] = property(*descrips)
-                else:
-                    temp_classdict[new_attr] = property(None, classdict[class_obj])
 
+        for class_obj in classdict:
+            new_descriptor = {}
+            new_attr = ''
+
+            if re.fullmatch(get_regexp, class_obj) or re.fullmatch(set_regexp, class_obj):
+                operation, attr, *_ = class_obj.split('_')
+                new_descriptor[operation] = classdict[class_obj]
+                new_attr = attr
+
+            old_descriptor = cls._get_descriptors(temp_classdict.get(new_attr))
+            if old_descriptor:
+                new_descriptor = {**old_descriptor, **new_descriptor}
+
+            if new_attr:
+                temp_classdict[new_attr] = property(
+                    fget=new_descriptor.get('get'),
+                    fset=new_descriptor.get('set'),
+                    fdel=new_descriptor.get('delete'),
+                )
         classdict = {**classdict, **temp_classdict}
         return type.__new__(cls, name, bases, classdict)
 
     @staticmethod
     def _get_descriptors(prop):
-        descriptors = []
+        """Get descriptors from property."""
+        descriptor = {}
         if isinstance(prop, property):
-            descriptors.append(prop.fget)
-            descriptors.append(prop.fset)
-            descriptors.append(prop.fdel)
-        return descriptors
-
-
-class New(metaclass=MetaNew):
-    def __init__(self, x):
-        self._x = x
-
-    def get_x(self):
-        return self._x
-
-    def set_x(self, val):
-        self._x = val
-        print('set val')
-
-    def get_y(self):
-        return 'y'
-
-a = New(42)
-print(a.x)
-a.x = 33
-print(a.x)
-print(a.y)
+            descriptor['get'] = prop.fget
+            descriptor['set'] = prop.fset
+            descriptor['delete'] = prop.fdel
+        return descriptor
